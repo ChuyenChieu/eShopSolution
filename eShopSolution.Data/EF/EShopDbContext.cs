d784d7e4be4d4701d0f868bb64514ed04648eb7b
Chuyền Chiêu <nguyenchuyen091292@gmail.com>
2020-04-17 10:09:42 +0700

    seeding_database

--- eShopSolution.Data/EF/EShopDbContext.cs
+++ eShopSolution.Data/EF/EShopDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using eShopSolution.Data.Configurations;
 using eShopSolution.Data.Entities;
+using eShopSolution.Data.Extensions;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -15,6 +16,7 @@ namespace eShopSolution.Data.EF
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            //Configure using Fluent API
             modelBuilder.ApplyConfiguration(new CartConfiguration());
 
             modelBuilder.ApplyConfiguration(new AppConfigConfiguration());
@@ -30,7 +32,13 @@ namespace eShopSolution.Data.EF
             modelBuilder.ApplyConfiguration(new ProductTranslationConfiguration());
             modelBuilder.ApplyConfiguration(new PromotionConfiguration());
             modelBuilder.ApplyConfiguration(new TransactionConfiguration());
-
+            //Data seeding
+            //modelBuilder.Entity<AppConfig>().HasData(
+            //    new AppConfig() { Key = "HomeTitle", Value = "This is home page of eShopSolution"},
+            //    new AppConfig() { Key = "HomeKeywork", Value = "This is keywork page of eShopSolution" },
+            //    new AppConfig() { Key = "HomeDescription", Value = "This is description page of eShopSolution" }
+            //    );
+            modelBuilder.Seed();
             //base.OnModelCreating(modelBuilder);
         }
 
