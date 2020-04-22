9d2d30546ee4c79308d7271a78c4d9c53123c563
Chuyền Chiêu <nguyenchuyen091292@gmail.com>
2020-04-22 16:31:12 +0700

    login_jwt

---
+++ eShopSolution.Application/System/Users/IUserService.cs
@@ -0,0 +1,15 @@
+﻿using eShopSolution.ViewModels.System.Users;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eShopSolution.Application.System.Users
+{
+    public interface IUserService
+    {
+        Task<string> Authenticate(LoginRequest request); //Trả về trạng thái đăng nhập
+
+        Task<bool> Register(RegisterRequest request); //Đăng kí
+    }
+}
